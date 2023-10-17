a84a12b9b87843c988ec8d699ef88b5fdddaa9d8
Carr0t-Hub <79178536+Carr0t-Hub@users.noreply.github.com>
2023-10-17 14:42:44 +0800

    Initial Design

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description">
+
+    <title>DA-BAR Home Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+
+  <body class="vw-100 vh-100">
+    <div class="intro-header">
+      <div class="d-flex justify-content-center">  
+        <img class="img-fluid mt-4" src="img/logo_text.png" alt="DA-BAR" height="30%" width="30%">
+      </div>
+      <div class="container mt-2">
+        <div class="row">
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Online Bookshop</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Online Evaluation</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Online Exam</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Employees Portal</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="row mt-1">
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Dorm Reservation</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">E-Library</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">E-Newsletter</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">NAREA</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="row mt-1">
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Online Maya</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Online Registration</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">Seminar Registration</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-3">
+            <a class="alert button-color d-flex align-items-center" role="alert" href="#">
+              <img class="img-fluid flex-shrink-0 ms-3" src="img/logo.png" alt="" height="30%" width="30%">
+              <div class="flex-grow-1">
+                <h6 class="mb-0">TechFlix</h6>
+                <small>Description</small>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description">
+
+    <title>DA-BAR Home Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/styles.css" rel="stylesheet">
+  </head>
+
+  <body class="vw-100 vh-100">
+
+    <!-- Header -->
+    <header class="intro-header">
+      <div class="container">
+        <div class="intro-message">
+          <span class="logo"><img src="img/logo.png" alt="DA-BAR"></span>
+
+          <h3>Department of Agriculture</h3>
+          <h2>Bureau of Agricultural Research</h2>
+          
+          <hr class="intro-divider">
+            <ul class="list-inline intro-social-buttons">
+              <!-- <li class="list-inline-item">
+                <a href="compendium/login.php" class="btn btn-info btn-lg">
+                  <i class="fa fa-home fa-fw"></i>
+                  <span class="network-name">Compendium</span>
+                </a>
+              </li> -->
+              <li class="list-inline-item">
+                <a href="Online_Maya/index.php" class="btn btn-info btn-lg">
+                  <i class="fa fa-codepen fa-fw"></i>
+                  <span class="network-name">BAR Maya Program</span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="BARPortal/index.php" class="btn btn-primary btn-lg">
+                  <i class="fa fa-globe fa-fw"></i>
+                  <span class="network-name">BAR Portal</span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="BARBookShop/index.php" class="btn btn-success btn-lg">
+                  <i class="fa fa-book fa-fw"></i>
+                  <span class="network-name">Libreng Libro sa BAR</span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="UCAS/index.php" class="btn btn-warning btn-lg">
+                  <i class="fa fa-globe fa-fw"></i>
+                  <span class="network-name">DA-BAR UCAS</span>
+                </a>
+              </li>
+              <!-- <li class="list-inline-item">
+                <a href="itpgrfa/index.html" class="btn btn-info btn-lg">
+                  <i class="fa fa-codiepie fa-fw"></i>
+                  <span class="network-name">Rice Roots Legacy</span>
+                </a>
+              </li> -->
+            </ul>
+        </div>
+      </div>
+    </header>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
